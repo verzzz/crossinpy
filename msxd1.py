ebf33062e964e2045f77eeddb05069b7d7b627a0
verzz <miegaa.mon@gmail.com>
2017-04-27 10:44:58 +0800

    test file status

---
+++ msxd1.py
@@ -0,0 +1,14 @@
+a = 'hello,world'
+b = 1.41
+c = (1!=2)
+d = (True and False)
+e = (True or False)
+
+print (a,b,c,d,e)
+
+print ('a:%s;b:%.2f;c:%s;d:%s;e:%s' % (a,b,c,d,e))
+
+print('\'' +a + '\'')
+
+print ('\\' + '\"' + a + '\"')
+
